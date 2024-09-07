fead8685880600f0b37edc9194ddfe6cf9178462
hhlaxbro <123035611+hhlaxbro@users.noreply.github.com>
2024-09-07 15:27:22 -0500

    TutorialSetUp

--- GameJam/Assets/Scripts/UI_Controller.cs
+++ GameJam/Assets/Scripts/UI_Controller.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.InputSystem;
+using System;
 
 public class UI_Controller : MonoBehaviour
 {
@@ -15,16 +16,56 @@ public class UI_Controller : MonoBehaviour
     public Image TutorialIcon;
     public TMP_Text Title;
     public TMP_Text Body;
+    private int CurrentPage;
 
     public PlayerInput playerInput;
     private InputAction navigateForward;
     private InputAction navigateBackwards;
+    public GameObject BeginButton;
 
     private void Start()
     {
         playerInput.currentActionMap.Enable();
         navigateForward = playerInput.currentActionMap.FindAction("Progress");
         navigateBackwards = playerInput.currentActionMap.FindAction("MoveBack");
+        navigateBackwards.performed += NavigateBackwards_performed;
+        navigateForward.performed += NavigateForward_performed;
+    }
+
+    private void NavigateForward_performed(InputAction.CallbackContext obj)
+    {
+        if(CurrentPage < TutorialPages.Length - 1)
+        {
+            CurrentPage = CurrentPage + 1;
+            DisplayPage();
+
+            if (CurrentPage == TutorialPages.Length - 1)
+            {
+                BeginButton.SetActive(true);
+            }
+        }
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+    private void DisplayPage()
+    {
+        Title.text = TutorialPages[CurrentPage].PageTitle;
+        Body.text = TutorialPages[CurrentPage].BodyText;
+        TutorialIcon.sprite = TutorialPages[CurrentPage].PageIcon;
+    }
+
+    private void NavigateBackwards_performed(InputAction.CallbackContext obj)
+    {
+        if(CurrentPage > 0) 
+        { 
+            CurrentPage = CurrentPage - 1; 
+            DisplayPage();
+            BeginButton.SetActive(false);
+        }
     }
 
     public void CreditButtonPress() 
@@ -45,6 +86,9 @@ public class UI_Controller : MonoBehaviour
     { 
     TutorialCanvas.SetActive(true);
     MainMenuCanvas.SetActive(false);
-    CreditsCanvas.SetActive(false) ;
+    CreditsCanvas.SetActive(false);
+
+    CurrentPage = 0;
+    DisplayPage();
     }
 }
