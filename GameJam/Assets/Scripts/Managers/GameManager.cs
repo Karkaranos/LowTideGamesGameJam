28a524f5f666c43f2e5f738f057eeb44fdbf4280
kaptainKirby <rherwig@mail.bradley.edu>
2024-09-06 19:00:22 -0500

    push

---
+++ GameJam/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        
+    }
+}
