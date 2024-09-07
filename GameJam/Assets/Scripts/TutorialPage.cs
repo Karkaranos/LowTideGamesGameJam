f44d9df96a4b7c62b6d8d10c056570a7b5e30343
Karkaranos <karkaranosyt@gmail.com>
2024-09-07 14:21:37 -0500

    tutorial page

---
+++ GameJam/Assets/Scripts/TutorialPage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPage
+{
+    public string PageTitle;
+    public string BodyText;
+    public Sprite PageIcon;
+}
