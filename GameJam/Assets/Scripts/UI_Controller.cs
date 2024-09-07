76041bc40f953f10237e4d2e6eef20a449595ed5
hhlaxbro <123035611+hhlaxbro@users.noreply.github.com>
2024-09-07 14:22:34 -0500

    Tutorial_Button

--- GameJam/Assets/Scripts/UI_Controller.cs
+++ GameJam/Assets/Scripts/UI_Controller.cs
@@ -7,16 +7,26 @@ public class UI_Controller : MonoBehaviour
 {
     public GameObject CreditsCanvas;
     public GameObject MainMenuCanvas;
+    public GameObject TutorialCanvas;
 
     public void CreditButtonPress() 
     {
         CreditsCanvas.SetActive(true);
         MainMenuCanvas.SetActive(false);
+        TutorialCanvas.SetActive(false);
     }
 
     public void BackToMainMenu()
     {
         CreditsCanvas.SetActive(false);
         MainMenuCanvas.SetActive(true);
+        TutorialCanvas.SetActive(false);
+    }
+
+    public void StartGameTutorial()
+    { 
+    TutorialCanvas.SetActive(true);
+    MainMenuCanvas.SetActive(false);
+    CreditsCanvas.SetActive(false) ;
     }
 }
